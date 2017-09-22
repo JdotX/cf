43f63786b33a9127e9e259cf9c1295f1ac3b762e
jdotx <jxiaab@ust.hk>
2017-09-22 19:08:37 +0800

    sleepy

--- 845/845C.cpp
+++ 845/845C.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
- 
+#include <set> 
+
 using namespace std;
- 
+
+const int maxn = 200010;
+
+vector<int> adj_list[maxn];
+set<int> beauty[maxn];
+int a[maxn];
+
+int gcd(int x, int y) {
+  while (y) {
+    int t = x;
+    x = y;
+    y = t % y;
+  }
+  return x;
+}
+
+void dfs(int node, int parent, int prev_all_gcd)
+{
+    /* current node selected */
+    for (auto b: beauty[parent])
+        beauty[node].insert(gcd(a[node], b));
+    /* current node not selected */
+    beauty[node].insert(prev_all_gcd);
+
+    int all_gcd = gcd(a[node], prev_all_gcd);
+    for (auto next: adj_list[node]) {
+        if (next != parent)
+            dfs(next, node, all_gcd);
+    }
+    return;
+}
+
 int main()
 {
-    int n;
+  int n;
     cin >> n;
-    vector<pair<int, int> > b(n);
-    long long t0 = -1, t1 = -1;
-    for (int i = 0; i < n; i++)
-        cin >> b[i].first >> b[i].second;
- 
-    sort(b.begin(), b.end());
-    for(int i = 0; i < n; i++) {
-        if( t0 < b[i].first)
-            t0 = b[i].second;
-        else if (t1 < b[i].first)
-            t1 = b[i].second;
-        else {
-            cout << "NO" << endl;
-            return 0;
-        }
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n - 1; i++) {
+        int x, y;
+        cin >> x >> y;
+        adj_list[x].push_back(y);
+        adj_list[y].push_back(x);
     }
- 
-    cout << "YES" << endl;
+    int root = 1, parent = 0;
+    beauty[0].insert(0);
+    dfs(root, parent, 0);
+    for (int i = 1; i <= n; i++)
+        cout << *(beauty[i].rbegin()) << " ";
+    cout << endl;
+    return 0;
 }
